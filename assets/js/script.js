8a3faa650110f9d7d78c7b322f9f5d64b3f1afd0
Garrett <garrettleegrahamanderson@gmail.com>
2022-12-07 20:46:37 -0500

    got the api to return data for the city lat and long coords

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@ var cityName
 var cityLat
 var cityLong
 var searchCityInput = $("#search-for-city")
+var searchCityBtn = $("#search-city-button")
 var apiKey = "189a38ae6bf0bf147aa5670c0b4b70d5"
-var cityCoords = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
-var cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLong}&appid=${apiKey}`
+var cityCoords = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+var cityWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLong}&appid=${apiKey}`
 
 // when city is searched for with the input, get the geo location of that city
     // make a fetch call to the api for the specific city
@@ -17,3 +18,22 @@ var cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=$
 // when search button is clicked, save city name in variable
     // make api call with city name on cityCoords variable
     // get the lat and long coords of city in a variable
+
+
+function getCityCoords() {
+    cityName = searchCityInput.val()
+    console.log()
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+      .then(function (response) {
+        console.log(response)
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(data)
+
+        // save the city lat and long to variables
+        // fetch weather data with lat and long variables
+      })
+}
+
+searchCityBtn.on('click', getCityCoords)
